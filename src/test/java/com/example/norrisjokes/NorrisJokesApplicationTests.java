48e6001d69c078d7e72b300be956dabeefd1802f
Abdullahi <abhas955@gmail.com>
2022-08-30 23:08:56 -0400

    initial commit

---
+++ src/test/java/com/example/norrisjokes/NorrisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.norrisjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorrisJokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
